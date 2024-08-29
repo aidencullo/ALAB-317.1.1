b825b58295b04754b3a9b995f8ce9041432946f7
Aiden Cullo <aidencullo@gmail.com>
2024-08-29 14:14:06 -0400

    fix ts errors

--- src/index.ts
+++ src/index.ts
@@ -1,47 +1,52 @@
 // @ts-check
 
-class Vehicle {
-  status = "stopped";
+type StatusUnion = "started" | "stopped";
 
-  constructor(make, model, wheels) {
-    this.make = make;
-    this.model = model;
-    this.wheels = wheels;
-  }
-  start() {
-    this.status = "started";
-  }
-  stop() {
-    this.status = "stopped";
-  }
+class Vehicle {
+    status: StatusUnion = "stopped";
+    make: string;
+    model: string;
+    wheels: number;
+
+    constructor(make: string, model: string, wheels: number) {
+	this.make = make;
+	this.model = model;
+	this.wheels = wheels;
+    }
+    start() {
+	this.status = "started";
+    }
+    stop() {
+	this.status = "stopped";
+    }
 }
 
 class Car extends Vehicle {
-  constructor(make, model) {
-    super(make, model, "four");
-  }
+    constructor(make: string, model: string) {
+	super(make, model, 4);
+    }
 }
 
 class MotorCycle extends Vehicle {
-  constructor(make, model) {
-    super(make, model, 2);
-  }
+    constructor(make: string, model: string) {
+	super(make, model, 2);
+    }
 }
 
-function printStatus(vehicle) {
-  if (vehicle.status === "running") {
-    console.log("The vehicle is running.");
-  } else {
-    console.log("The vehicle is stopped.");
-  }
+function printStatus(vehicle: Vehicle) {
+    if (vehicle.status === "started") {
+	console.log("The vehicle is running.");
+    } else {
+	console.log("The vehicle is stopped.");
+    }
 }
 
 const myHarley = new MotorCycle("Harley-Davidson", "Low Rider S");
 myHarley.start();
 printStatus(myHarley);
-console.log(myHarley.make.toUppercase());
+console.log(myHarley.make.toUpperCase());
 
 const myBuick = new Car("Buick", "Regal");
 myBuick.wheels = myBuick.wheels - 1;
 console.log(myBuick.wheels);
-console.log(myBuick.mdl);
+console.log(myBuick.model);
